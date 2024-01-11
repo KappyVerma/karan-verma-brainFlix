b3bcf1a85728f245a3915335f9ae33b8918ad185
Karan Verma <karranverma@gmail.com>
2024-01-11 00:03:31 -0800

    add if statement in eventhandler, removed props from app.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import avatar from "./assets/Images/Mohan-muruge.jpg";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import VideoUpload from "./pages/VideoUpload/VideoUpload";
-import videoPreview from "./assets/Images/Upload-video-preview.jpg";
 
 function App() {
   return (
@@ -16,21 +15,7 @@ function App() {
         <Route path="/video/:id" element={<HomePage url={url} />} />
         <Route
           path="/videoUpload"
-          element={
-            <VideoUpload
-              url={url}
-              avatar={avatar}
-              heading="Upload Video"
-              image={videoPreview}
-              imageLabel="video thumbnail"
-              titleLabel="title your video"
-              titlePlaceHolder="Add a title to your video"
-              descriptionLabel="Add a video description"
-              descPlaceHolder="Add a description to your video"
-              button="publish"
-              buttonText="cancel"
-            />
-          }
+          element={<VideoUpload url={url} avatar={avatar} />}
         />
       </Routes>
     </BrowserRouter>
--- src/pages/VideoUpload/VideoUpload.jsx
+++ src/pages/VideoUpload/VideoUpload.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
+import videoPreview from "../../assets/Images/Upload-video-preview.jpg";
 import "./VideoUpload.scss";
 
 function VideoUpload(props) {
@@ -10,36 +11,43 @@ function VideoUpload(props) {
   const Navigate = useNavigate();
 
   const handleTitleChange = (event) => {
+    event.target.style.border = "";
     setTitle(event.target.value);
   };
   const handleDescriptionChange = (event) => {
+    event.target.style.border = "";
     setDescription(event.target.value);
   };
 
   async function eventHandler(event) {
     event.preventDefault();
-    try {
-      await axios.post(`${props.url}/videos`, {
-        title: title,
-        description: description,
-      });
-      alert("Video uploaded successfully");
-      Navigate("/");
-    } catch (error) {
-      console.log(error);
+    if (!title || !description) {
+      event.target.title.style.border = "1px solid #d22d2d";
+      event.target.description.style.border = "1px solid #d22d2d";
+    } else {
+      try {
+        await axios.post(`${props.url}/videos`, {
+          title: title,
+          description: description,
+        });
+        alert("Video uploaded successfully");
+        Navigate("/");
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 
   return (
     <>
       <section className="video">
-        <h1 className="video__heading">{props.heading}</h1>
+        <h1 className="video__heading">Upload Video</h1>
 
         <div className="video__container">
           <div className="video__block">
-            <h4 className="video__label">{props.imageLabel} </h4>
+            <h4 className="video__label">video thumbnail</h4>
             <img
-              src={props.image}
+              src={videoPreview}
               alt="video-preview"
               className="video__preview"
             />
@@ -48,24 +56,24 @@ function VideoUpload(props) {
           <div className="video__form">
             <form onSubmit={eventHandler} id="form">
               <label className="video__label">
-                {props.titleLabel}
+                title your video
                 <input
                   onChange={handleTitleChange}
                   className="video__input"
                   type="text"
                   name="title"
-                  placeholder={props.titlePlaceHolder}
+                  placeholder="Add a title to your video"
                   value={title}
                 />
               </label>
               <label className="video__label">
-                {props.descriptionLabel}
+                Add a video description
                 <textarea
                   onChange={handleDescriptionChange}
                   className="video__textArea"
                   type="text"
                   name="description"
-                  placeholder={props.descPlaceHolder}
+                  placeholder="Add a description to your video"
                   value={description}
                 />
               </label>
@@ -74,9 +82,9 @@ function VideoUpload(props) {
         </div>
         <div className="video__button-section">
           <button className="video__button" form="form">
-            {props.button}
+            publish
           </button>
-          <p className="video__button-secondary">{props.buttonText}</p>
+          <p className="video__button-secondary">cancel</p>
         </div>
       </section>
     </>
